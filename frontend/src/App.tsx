0b8b9433fef26cadfd037bd13497e4aeb966ab3f
Alex Andrade <webandrade@gmail.com>
2022-01-14 12:44:42 -0300

    Navigation buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Listing />} />
-        <Route path="/form">
+        <Route path="/Form">
           <Route path=":movieId" element={<Form />} />
         </Route>
       </Routes>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -24,8 +24,7 @@ function Listing() {
                         <MovieCard />
                     </div>
                 </div>
-            </div> 
-  
+            </div>   
         </>
     );
 }
